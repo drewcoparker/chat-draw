3e71ec60d9acaa4507d89c520c88ffe18c2312d2
Drew Parker <drew.i.parker@gmail.com>
2017-01-31 14:20:13 -0500

    Adding canvas

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ var http = require("http");
 var fs = require("fs");
 
 
+
 var server = http.createServer((req, res) => {
-    console.log("Someone connected via http");
-    fs.readFile("index.html", "utf-8", (error, fileData) => {
-        if (error) {
-            // Respond with a 500 if erroneous
-            res.writeHead(500, {"content-type": "text/html"});
-            res.end(error);
-        } else {
-            res.writeHead(200, {"content-type": "text/html"});
-            res.end(fileData);
-        }
-    });
+    // console.log("Someone connected via http");
+    // fs.readFile("index.html", "utf-8", (error, fileData) => {
+    //     if (error) {
+    //         // Respond with a 500 if erroneous
+    //         res.writeHead(500, {"content-type": "text/html"});
+    //         res.end(error);
+    //     } else {
+    //         res.writeHead(200, {"content-type": "text/html"});
+    //         res.end(fileData);
+    //     }
+    // });
 });
 
 var socketIo = require("socket.io");
@@ -40,4 +41,4 @@ io.sockets.on("connect", (socket) => {
 });
 
 server.listen(8080);
-console.log("Listening on port 8000");
+console.log("Listening on port 8080");
