2a591d43ebd1474d6cd77857cad2d5b53d79b867
Drew Parker <drew.i.parker@gmail.com>
2017-01-31 12:00:12 -0500

    Added client message socket method

--- index.js
+++ index.js
@@ -21,5 +21,23 @@ var socketIo = require("socket.io");
 // Sockets are going to listen to the server which is listening to port 8080.
 var io = socketIo.listen(server);
 
+// Handle socket connections...
+var socketUsers = [];
+io.sockets.on("connect", (socket) => {
+    console.log("Socket connection!!");
+    socketUsers.push({
+        socketID: socket.id,
+        name: "Anonymous"
+    });
+    io.sockets.emit("users", socketUsers);
+    socket.on("messageToServer", (messageObject) => {
+        console.log(`Someone's message: "${messageObject.message}"`);
+        io.sockets.emit("messageToClient", {
+            message: messageObject.message,
+            date: new Date()
+        })
+    })
+});
+
 server.listen(8080);
 console.log("Listening on port 8000");
